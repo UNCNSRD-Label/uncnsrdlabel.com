344494336d2ff3ec32ea7312691ad0b4af708baf
Karl Podger <karl.podger@primeordinal.com>
2022-09-06 15:59:05 +0100

    Add Google Tag Manager script and noscript tags

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 import Head from "next/head";
+import Script from "next/script";
 
 import Header from "@components/header";
 import Footer from "@components/footer";
@@ -57,6 +58,30 @@ export const Layout: React.FC<Props> = ({
         <main className={clsx(styles.main)}>{children}</main>
         {showHeaderAndFooter && <Footer />}
       </div>
+
+      {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
+        <>
+          <Script
+            id="google-tag-manager"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`,
+            }}
+          />
+
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+        </>
+      )}
     </>
   );
 };
