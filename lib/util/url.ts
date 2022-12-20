7b5c57a12776b389cd58e4b51455fae482b896b9
Karl Podger <karl.podger@primeordinal.com>
2022-12-20 05:48:27 +0000

    Remove Vercel URL from URL helper

--- lib/util/url.ts
+++ lib/util/url.ts
@@ -6,9 +6,9 @@ import type { PartialDeep } from "type-fest";
 
 export const getProductURL = (
   product: PartialDeep<Product, { recurseIntoArrays: true }>
-) => `${process.env.NEXT_PUBLIC_VERCEL_URL}/product/${product.handle}`;
+) => `/product/${product.handle}`;
 
 export const getProductVariantURL = (
   product: PartialDeep<Product, { recurseIntoArrays: true }>,
   productVariant: PartialDeep<ProductVariant, { recurseIntoArrays: true }>
-) => `${process.env.NEXT_PUBLIC_VERCEL_URL}/product/${product.handle}`;
+) => `/product/${product.handle}`;
