5fa0ad6dc295e3cf273b6450fec8202027d75fb0
Karl Podger <karl.podger@primeordinal.com>
2023-01-04 22:51:10 +0000

    Refine styling of Consent component

--- components/Consent/index.tsx
+++ components/Consent/index.tsx
@@ -73,37 +73,15 @@ export const Component: FC<Props> = ({ className, data, route, view }) => {
     return null;
   }
 
-  console.log({ dismiss, route });
-
   return (
     <div className={clsx(className, styles.root, dismiss && "hidden")}>
-      <div className={clsx("collapse", styles.information)}>
-        <div className={clsx(styles.about)}>
-          <Image
-            alt={`${data?.shop?.name} logo`}
-            className={clsx(styles.logo, "dark:invert")}
-            height={40}
-            src="/images/logos/logotype.svg"
-            width={180}
-          />
-          <p>We use some essential cookies to make this service work.</p>
-          <p>
-            We’d also like to use analytics cookies so we can understand how you
-            use the service and make improvements.
-          </p>
-        </div>
-
-        <div className={clsx("collapse", styles.manage)}>
-          <input className={clsx(styles.toggle)} type="checkbox" />
-          <div className="collapse-title link underline-offset-4">
-            Manage consent settings
-          </div>
-          <div className={clsx("collapse-content")}>
-            <ConsentTable className={clsx("w-full")} />
-          </div>
-        </div>
-      </div>
-
+      <Image
+        alt={`${data?.shop?.name} logo`}
+        className={clsx(styles.logo, "dark:invert")}
+        height={40}
+        src="/images/logos/logotype.svg"
+        width={180}
+      />
       <button
         className={clsx("btn", "btn-ghost", styles.closeButton)}
         onClick={(e) => {
@@ -117,6 +95,25 @@ export const Component: FC<Props> = ({ className, data, route, view }) => {
         />
         <span className={clsx("sr-only")}>Close banner</span>
       </button>
+      <div className={clsx("collapse", styles.information)}>
+        <div className={clsx(styles.about)}>
+          <p>We use some essential cookies to make this service work.</p>
+          <p>
+            We’d also like to use analytics cookies so we can understand how you
+            use the service and make improvements.
+          </p>
+        </div>
+
+        <div className={clsx("collapse", styles.manage)}>
+          <input className={clsx(styles.toggle)} type="checkbox" />
+          <div className="collapse-title link underline-offset-4">
+            Manage consent settings
+          </div>
+          <div className={clsx(styles.tableContainer, "collapse-content")}>
+            <ConsentTable className={clsx(styles.table, "w-full")} />
+          </div>
+        </div>
+      </div>
 
       <div
         className={clsx(
