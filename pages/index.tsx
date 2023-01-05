0d9b72c0ed2a4ec5e589dd7fe0337e3eb18f4d19
Karl Podger <karl.podger@primeordinal.com>
2023-01-05 14:12:05 +0000

    Add next-seo to set title and description of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { request } from "graphql-request";
 import Error from "next/error";
 import Image from "next/image";
 import Link from "next/link";
+import { NextSeo } from "next-seo";
 
 import Layout from "#/components/Layout";
 
@@ -54,53 +55,63 @@ export default function Page({
   errors,
 }: StorefrontApiResponseOk<HomeQuery>) {
   return (
-    <Layout
-      classNameDrawerContent={clsx(
-        styles.drawerContent,
-        "drawerContentOverflowY"
+    <>
+      {" "}
+      {data && (
+        <NextSeo title={data.shop?.name} description={data.shop?.description} />
       )}
-      classNameMain={clsx(styles.main)}
-      data={data}
-      showHeaderAndFooter={true}
-    >
-      <div className={clsx(styles.root, "fitViewport")}>
-        <figure className={clsx(styles.figure)}>
-          <Link href="/campaign" className={clsx(styles.link)} title="Shop now">
-            <Image
-              alt="Sexy young woman posing in bikini on hotel bathroom counter"
-              className={clsx(
-                styles.image,
-                "onLoadingComplete",
-                "tablet:hidden"
-              )}
-              fill
-              onLoadingComplete={onLoadingComplete}
-              priority
-              src="/images/art/lp1.jpg"
-            />
-            <Image
-              alt="Sexy young woman posing in bikini on hotel bathroom counter"
-              className={clsx(
-                styles.image,
-                "onLoadingComplete",
-                "hidden",
-                "tablet:block"
-              )}
-              fill
-              onLoadingComplete={onLoadingComplete}
-              priority
-              src="/images/art/desktop.jpg"
-            />
+      <Layout
+        classNameDrawerContent={clsx(
+          styles.drawerContent,
+          "drawerContentOverflowY"
+        )}
+        classNameMain={clsx(styles.main)}
+        data={data}
+        showHeaderAndFooter={true}
+      >
+        <div className={clsx(styles.root, "fitViewport")}>
+          <figure className={clsx(styles.figure)}>
+            <Link
+              href="/campaign"
+              className={clsx(styles.link)}
+              title="Shop now"
+            >
+              <Image
+                alt="Sexy young woman posing in bikini on hotel bathroom counter"
+                className={clsx(
+                  styles.image,
+                  "onLoadingComplete",
+                  "tablet:hidden"
+                )}
+                fill
+                onLoadingComplete={onLoadingComplete}
+                priority
+                src="/images/art/lp1.jpg"
+              />
+              <Image
+                alt="Sexy young woman posing in bikini on hotel bathroom counter"
+                className={clsx(
+                  styles.image,
+                  "onLoadingComplete",
+                  "hidden",
+                  "tablet:block"
+                )}
+                fill
+                onLoadingComplete={onLoadingComplete}
+                priority
+                src="/images/art/desktop.jpg"
+              />
+            </Link>
+          </figure>
+          <Link
+            href="/campaign"
+            className={clsx(styles.cta, "cta", "text-sm")}
+            title="View the campaign"
+          >
+            View the campaign
           </Link>
-        </figure>
-        <Link
-          href="/campaign"
-          className={clsx(styles.cta, "cta", "text-sm")}
-          title="View the campaign"
-        >
-          View the campaign
-        </Link>
-      </div>
-    </Layout>
+        </div>
+      </Layout>
+    </>
   );
 }
