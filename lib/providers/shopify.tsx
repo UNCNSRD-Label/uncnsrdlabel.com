898341ad6656db11dbd1beeaa956a7c15eeb7e11
Karl Podger <karl.podger@primeordinal.com>
2023-03-01 10:35:58 +0000

    Pass CountryCode to CartProvider

--- lib/providers/shopify.tsx
+++ lib/providers/shopify.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import type { ShopifyContextProps } from "@shopify/hydrogen-react/dist/types/ShopifyProvider";
+
 import { ShopifyProvider, CartProvider } from "@shopify/hydrogen-react";
-import { ShopifyContextProps } from "@shopify/hydrogen-react/dist/types/ShopifyProvider";
+import { CountryCode } from "@shopify/hydrogen-react/storefront-api-types";
 import { ReactNode } from "react";
 
 export default function Provider({
@@ -11,9 +13,13 @@ export default function Provider({
   children: ReactNode;
   shopifyConfig: ShopifyContextProps;
 }) {
+  const locale = new Intl.Locale(shopifyConfig.locale!);
+
   return (
     <ShopifyProvider shopifyConfig={shopifyConfig}>
-      <CartProvider>{children}</CartProvider>
+      <CartProvider countryCode={locale.region as CountryCode}>
+        {children}
+      </CartProvider>
     </ShopifyProvider>
   );
 }
