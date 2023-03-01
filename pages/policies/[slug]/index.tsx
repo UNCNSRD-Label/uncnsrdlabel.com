f23ec62e1c69981b3afea6fe55f1436cce77c1ad
Karl Podger <karl.podger@primeordinal.com>
2023-03-01 10:35:11 +0000

    Rename PrivacyPolicy query to Policies

--- pages/policies/[slug]/index.tsx
+++ pages/policies/[slug]/index.tsx
@@ -1,7 +1,7 @@
 import type { StorefrontApiResponseOk } from "@shopify/hydrogen-react";
 import type { GetServerSideProps } from "next";
 
-import type { PrivacyPolicyQuery } from "#/generated/graphql/graphql";
+import type { PoliciesQuery } from "#/generated/graphql/graphql";
 
 import { clsx } from "clsx";
 import { request } from "graphql-request";
@@ -60,7 +60,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 export default function Page({
   data,
   errors,
-}: StorefrontApiResponseOk<PrivacyPolicyQuery>) {
+}: StorefrontApiResponseOk<PoliciesQuery>) {
   const { query } = useRouter();
 
   const scrollingElement = createRef<HTMLDivElement>();
