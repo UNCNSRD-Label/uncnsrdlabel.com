2737fdeb42663578c90d548f2be20d1ab5d9cd95
Karl Podger <karl.podger@primeordinal.com>
2023-05-14 02:30:10 +0100

    feat: Add video player instance to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from 'components/carousel';
 import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
+import { Video, type VideoProps } from 'components/video';
 import { Suspense } from 'react';
 
 export const runtime = 'edge';
@@ -20,9 +21,33 @@ export const metadata = {
   }
 };
 
+const video: VideoProps = {
+  alt: 'aaa',
+  title: 'bbb',
+  url: 'https://test-videos.co.uk/vids/jellyfish/mp4/h264/1080/Jellyfish_1080_10s_30MB.mp4'
+};
+
 export default async function HomePage() {
   return (
     <>
+      <Video
+        {...video}
+        autoPlay
+        // fallback={
+        //   <Image
+        //     alt={video.alt}
+        //     className="h-full object-contain"
+        //     fill
+        //     sizes="33vw"
+        //     src="https://i.ytimg.com/vi/M4V_61_kUbs/maxresdefault.jpg"
+        //     title={video.title}
+        //   />
+        // }
+        // fallback={<span>Cannot play video</span>}
+        loop
+        playsinline
+        // poster="https://i.ytimg.com/vi/M4V_61_kUbs/maxresdefault.jpg"
+      />
       {/* @ts-expect-error Server Component */}
       <ThreeItemGrid />
       <Suspense>
---
+++ components/video/index.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { type ReactPlayerProps } from 'react-player';
+import ReactPlayer from 'react-player/lazy';
+
+import { FC } from 'react';
+
+export type VideoProps = ReactPlayerProps;
+
+export const Video: FC<VideoProps> = ({
+  autoPlay = false,
+  className,
+  contentType,
+  fallback,
+  loop = false,
+  //   poster,
+  ...props
+}) => (
+  <figure className={'relative aspect-video'}>
+    <ReactPlayer
+      {...props}
+      autoPlay={autoPlay}
+      className={'absolute inset-0'}
+      fallback={fallback}
+      height="100%"
+      light={!autoPlay && props.poster}
+      loop={loop}
+      muted={autoPlay}
+      playing={autoPlay}
+      playsinline
+      pip
+      //   poster={poster}
+      stopOnUnmount={false}
+      style={{
+        aspectRatio: props.width && props.height ? `${props.width} / ${props.height}` : undefined
+      }}
+      width="100%"
+    />
+    {props.title && <figcaption>{props.title}</figcaption>}
+  </figure>
+);
+
+export default Video;
