ec1d374fcf2bc007d7072bc33f158e9599267a60
Karl Podger <karl.podger@primeordinal.com>
2023-05-23 01:35:51 +0100

    wip: Remove inline margin from search layout

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -7,11 +7,11 @@ import { Suspense } from 'react';
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <div className="mx-auto flex max-w-7xl flex-col bg-white py-6 text-black dark:bg-black dark:text-white md:flex-row">
+      <div className="mx-auto flex flex-col bg-white py-6 text-black dark:bg-black dark:text-white md:flex-row">
         <div className="order-first flex-none md:w-1/6">
           <Collections />
         </div>
-        <div className="order-last min-h-screen w-full md:order-none">{children}</div>
+        <div className="order-last min-h-screen w-full max-w-7xl md:order-none">{children}</div>
         <div className="order-none md:order-last md:w-1/6 md:flex-none">
           <FilterList list={sorting} title="Sort by" />
         </div>
