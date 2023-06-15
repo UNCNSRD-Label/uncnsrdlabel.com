a990fe515338246822d5d30b21f2b4cf551da708
Karl Podger <karl.podger@primeordinal.com>
2023-06-16 00:57:50 +0100

    chore: Extend Tailwind's aspect ratios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,83 @@
-const plugin = require('tailwindcss/plugin');
-const colors = require('tailwindcss/colors');
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './icons/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./icons/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       aspectRatio: {
-        '3/4': '3 / 4',
-        '4/3': '4 / 3'
+        "16/9": "16 / 9",
+        "2/3": "2 / 3",
+        "3/2": "3 / 2",
+        "3/4": "3 / 4",
+        "4/3": "4 / 3",
       },
       fontFamily: {
-        brand: ['var(--font-bomber-escort)'],
-        sans: ['var(--font-montserrat)']
+        brand: ["var(--font-bomber-escort)"],
+        sans: ["var(--font-montserrat)"],
       },
       fontSize: {
         xxs: [
-          '0.625rem',
+          "0.625rem",
           {
-            lineHeight: '0.675rem'
-          }
-        ]
+            lineHeight: "0.675rem",
+          },
+        ],
       },
       colors: {
         gray: colors.neutral,
-        hotPink: '#FF1966',
-        dark: '#111111',
-        light: '#FAFAFA',
-        violetDark: '#4c2889'
+        hotPink: "#FF1966",
+        dark: "#111111",
+        light: "#FAFAFA",
+        violetDark: "#4c2889",
       },
       keyframes: {
         fadeIn: {
           from: { opacity: 0 },
-          to: { opacity: 1 }
+          to: { opacity: 1 },
         },
         marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' }
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
         blink: {
-          '0%': { opacity: 0.2 },
-          '20%': { opacity: 1 },
-          '100% ': { opacity: 0.2 }
-        }
+          "0%": { opacity: 0.2 },
+          "20%": { opacity: 1 },
+          "100% ": { opacity: 0.2 },
+        },
       },
       animation: {
-        fadeIn: 'fadeIn .3s ease-in-out',
-        carousel: 'marquee 60s linear infinite',
-        blink: 'blink 1.4s both infinite'
-      }
-    }
+        fadeIn: "fadeIn .3s ease-in-out",
+        carousel: "marquee 60s linear infinite",
+        blink: "blink 1.4s both infinite",
+      },
+    },
   },
   future: {
-    hoverOnlyWhenSupported: true
+    hoverOnlyWhenSupported: true,
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
     plugin(({ matchUtilities, theme }) => {
       matchUtilities(
         {
-          'animation-delay': (value) => {
+          "animation-delay": (value) => {
             return {
-              'animation-delay': value
+              "animation-delay": value,
             };
-          }
+          },
         },
         {
-          values: theme('transitionDelay')
+          values: theme("transitionDelay"),
         }
       );
-    })
-  ]
+    }),
+  ],
 };
