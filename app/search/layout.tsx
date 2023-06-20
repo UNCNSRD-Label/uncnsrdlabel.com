690131808e3e819092ae56ed13085cc9fcc37b71
Karl Podger <karl.podger@primeordinal.com>
2023-06-20 13:15:21 +0100

    Fix: Prevent search page width collapsing on initial load

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,16 +1,22 @@
-import Collections from 'components/layout/search/collections';
-import FilterList from 'components/layout/search/filter';
-import { sorting } from 'lib/constants';
-import { Suspense } from 'react';
+import Collections from "components/layout/search/collections";
+import FilterList from "components/layout/search/filter";
+import { sorting } from "lib/constants";
+import { Suspense } from "react";
 
-export default function SearchLayout({ children }: { children: React.ReactNode }) {
+export default function SearchLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Suspense>
-      <div className="mx-auto flex flex-col bg-white py-6 text-black dark:bg-black dark:text-white md:flex-row">
+      <div className="mx-auto flex w-full flex-col bg-white py-6 text-black dark:bg-black dark:text-white md:flex-row">
         <div className="order-first flex-none md:w-1/6">
           <Collections />
         </div>
-        <div className="order-last min-h-screen w-full max-w-7xl md:order-none">{children}</div>
+        <div className="order-last min-h-screen w-full max-w-7xl md:order-none">
+          {children}
+        </div>
         <div className="order-none md:order-last md:w-1/6 md:flex-none">
           <FilterList list={sorting} title="Sort by" />
         </div>
