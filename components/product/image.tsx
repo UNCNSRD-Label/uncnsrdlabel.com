5e2715eb3ec4a02255cf6f58167410f481c81bfd
Karl Podger <karl.podger@primeordinal.com>
2023-06-25 14:20:37 +0100

    feat: Add zoom functionality to PDP images

--- components/product/image.tsx
+++ components/product/image.tsx
@@ -5,18 +5,26 @@ export function ProductImage({
   active,
   className,
   isInteractive = true,
+  scale = 1,
   ...props
 }: {
   active?: boolean;
   className?: string;
   isInteractive?: boolean;
+  scale?: number;
 } & React.ComponentProps<typeof Image>) {
   if (!props.src) {
     return null;
   }
 
   return (
-    <figure className={clsx("relative overflow-hidden", className)}>
+    <figure
+      className={clsx("relative overflow-hidden", className)}
+      style={{
+        // scale
+        width: `calc(100% * ${scale})`,
+      }}
+    >
       <Image
         className={clsx("relative block h-full w-full object-cover", {
           "transition duration-300 ease-in-out hover:scale-105": isInteractive,
--- components/product/images.tsx
+++ components/product/images.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import * as Slider from "@radix-ui/react-slider";
 import { useState } from "react";
 
 import { ProductImage } from "components/product/image";
@@ -11,34 +14,68 @@ export function Images({
   images: { src: string; altText: string }[];
   sizes?: string;
 }) {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [scale, setScale] = useState(1);
 
-  return (
+  return images.length > 1 ? (
     <>
-      {images.length > 1 ? (
-        <>
-          {images.map((image, index) => {
-            const isActive = index === currentImage;
-            return (
-              <button
-                aria-label="Enlarge product image"
-                key={image.src}
-                className="w-full"
-                onClick={() => setCurrentImage(index)}
-              >
+      {images.map((image) => {
+        return (
+          <Dialog.Root key={image.src}>
+            <Dialog.Trigger asChild>
+              <button aria-label="Enlarge product image" className="w-full">
                 <ProductImage
-                  active={isActive}
                   alt={image?.altText}
-                  className="aspect-3/4 h-full w-full"
+                  className="aspect-3/4 w-full"
                   fill
                   sizes={sizes}
                   src={image.src}
                 />
               </button>
-            );
-          })}
-        </>
-      ) : null}
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 z-50 bg-black">
+                <Dialog.Content className="data-[state=open]:animate-contentShow fixed inset-0 overflow-auto focus:outline-none">
+                  <ProductImage
+                    alt={image?.altText}
+                    className="aspect-3/4 w-full"
+                    fill
+                    quality={100}
+                    scale={scale}
+                    sizes="500vw"
+                    src={image.src}
+                  />
+                  <Slider.Root
+                    className="fixed bottom-8 right-8 flex h-5 w-64 touch-none select-none items-center"
+                    defaultValue={[1]}
+                    max={5}
+                    min={1}
+                    step={0.05}
+                    onValueChange={(value) => {
+                      setScale(value[0]);
+                    }}
+                  >
+                    <Slider.Track className="relative h-[3px] grow rounded-full bg-white/50">
+                      <Slider.Range className="absolute h-full rounded-full bg-white" />
+                    </Slider.Track>
+                    <Slider.Thumb
+                      className="block h-5 w-5 cursor-grab rounded-[10px] bg-white shadow-[0_1px_10px] shadow-black/70 focus:cursor-grabbing focus:outline-none"
+                      aria-label="Volume"
+                    />
+                  </Slider.Root>
+                  <Dialog.Close asChild>
+                    <button
+                      className="fixed right-8 top-8 inline-flex h-10 w-10 appearance-none items-center justify-center rounded-full p-2 focus:shadow-[0_0_0_1px] focus:shadow-white focus:outline-none"
+                      aria-label="Close"
+                    >
+                      <Cross2Icon className="h-full w-full text-white" />
+                    </button>
+                  </Dialog.Close>
+                </Dialog.Content>
+              </Dialog.Overlay>
+            </Dialog.Portal>
+          </Dialog.Root>
+        );
+      })}
     </>
-  );
+  ) : null;
 }
