753d04d42843ad7e1889e84b868965386bb928e9
Karl Podger <karl.podger@primeordinal.com>
2023-06-27 19:20:02 +0100

    chore: Reduce width of bag icon product count border

--- components/icons/cart.tsx
+++ components/icons/cart.tsx
@@ -12,7 +12,7 @@ export default function CartIcon({
     <div className="relative">
       <SlBag className={clsx("icon fill h-5 w-5", className)} />
       {quantity ? (
-        <div className="absolute bottom-0 left-0 -mb-3 -ml-3 flex h-5 w-5 items-center justify-center rounded-full border-2 border-white bg-black text-xs text-white dark:border-black dark:bg-white dark:text-black">
+        <div className="absolute bottom-0 left-0 -mb-3 -ml-3 flex h-5 w-5 items-center justify-center rounded-full border border-white bg-black text-xs">
           {quantity}
         </div>
       ) : null}
