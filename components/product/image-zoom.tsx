9e6555763770592b8adf25877337dc09ffc95634
Karl Podger <karl.podger@primeordinal.com>
2023-06-27 23:55:12 +0100

    wip: Refine product zoom overlay behaviour

--- components/product/image-zoom.tsx
+++ components/product/image-zoom.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import * as Slider from "@radix-ui/react-slider";
 import clsx from "clsx";
 import Image from "components/image";
+import LoadingDots from "components/loading-dots";
 import { useRef } from "react";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
@@ -19,8 +19,6 @@ export function ProductImageZoom({
 } & React.ComponentProps<typeof Image>) {
   const figureRef = useRef<HTMLElement>(null);
 
-  // const [scale, setScale] = useState(1);
-
   if (!props.src) {
     return null;
   }
@@ -30,6 +28,12 @@ export function ProductImageZoom({
       <TransformWrapper
         centerZoomedOut={false}
         limitToBounds={false}
+        onPanningStop={(ref, event) => {
+          console.log("onPanningStop", ref, event);
+          if (figureRef.current) {
+            figureRef.current.classList.remove("dragging");
+          }
+        }}
         onPanningStart={(ref, event) => {
           console.log("onPanningStart", ref, event);
           if (figureRef.current) {
@@ -40,39 +44,24 @@ export function ProductImageZoom({
         <TransformComponent>
           <figure
             className={clsx(
-              "relative w-[100dvw] cursor-grab [&.dragging]:cursor-grabbing",
+              "relative grid w-[100dvw] cursor-grab place-items-center bg-black [&.dragging]:cursor-grabbing",
               className
             )}
             ref={figureRef}
           >
             <Image
               className={clsx("relative block h-full w-full object-cover", {
-                "transition duration-300 ease-in-out": isInteractive,
+                "z-10 transition duration-300 ease-in-out": isInteractive,
               })}
               {...props}
               alt={props.title || ""}
             />
+            <div className="bg-red place-center absolute top-0 z-0 grid h-[100dvh]">
+              <LoadingDots className="h-4 w-4 bg-white" />
+            </div>
           </figure>
         </TransformComponent>
       </TransformWrapper>
-      <Slider.Root
-        className="fixed inset-x-8 bottom-8 flex h-5 touch-none select-none items-center sm:start-auto sm:w-48"
-        defaultValue={[1]}
-        max={5}
-        min={1}
-        step={0.05}
-        // onValueChange={(value) => {
-        //   setScale(value[0] ?? 1);
-        // }}
-      >
-        <Slider.Track className="relative h-[3px] grow rounded-full bg-white/50 drop-shadow">
-          <Slider.Range className="absolute h-full rounded-full bg-white " />
-        </Slider.Track>
-        <Slider.Thumb
-          className="block h-5 w-5 cursor-grab rounded-[10px] bg-white shadow-[0_1px_10px] shadow-black/70 focus:cursor-grabbing focus:outline-none"
-          aria-label="Volume"
-        />
-      </Slider.Root>
     </>
   );
 }
