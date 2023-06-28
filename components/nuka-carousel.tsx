2f04554faa059fb2a78959ffe22b35d9a06cdcf3
Karl Podger <karl.podger@primeordinal.com>
2023-06-28 01:45:15 +0100

    wip: Handle nuka carousel's slidesToShow responsively

--- components/nuka-carousel.tsx
+++ components/nuka-carousel.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useMediaQuery } from "@react-hookz/web";
 import Carousel, { type CarouselProps } from "nuka-carousel";
 import { Suspense } from "react";
 
 export type { CarouselProps as NukaCarouselProps };
 
 export async function NukaCarousel(props: CarouselProps) {
+  const isMobile = useMediaQuery("(max-width: 639px)");
+
+  const slidesToShow = isMobile ? 1 : props.slidesToShow ?? 3;
+
   return (
     <Suspense>
       <Carousel
@@ -20,6 +25,7 @@ export async function NukaCarousel(props: CarouselProps) {
             .closest(".slider-frame")
             ?.classList.add("dragging");
         }}
+        slidesToShow={slidesToShow}
       >
         {props.children}
       </Carousel>
