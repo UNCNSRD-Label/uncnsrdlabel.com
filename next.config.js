0d91ea112cf81d02897efe8f30adb43ba6954242
Karl Podger <karl.podger@primeordinal.com>
2023-06-30 13:56:16 +0100

    chore: Configure @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   eslint: {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true,
@@ -22,4 +26,4 @@ module.exports = {
       },
     ],
   },
-};
+});
