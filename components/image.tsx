62b4495058f42489a43dbfc9a4829467b2e58c4c
Karl Podger <karl.podger@primeordinal.com>
2023-07-03 22:01:09 +0100

    wip: Refine image loading animation

--- components/image.tsx
+++ components/image.tsx
@@ -1,17 +1,28 @@
 "use client";
 
+import { useIntersectionObserver } from "@react-hookz/web";
 import clsx from "clsx";
 import NextImage, { type ImageProps } from "next/image";
+import { useRef } from "react";
 
 import { onLoadingComplete } from "lib/image";
 
 export default function Image(props: ImageProps) {
+  const rootRef = useRef<HTMLDivElement>(null);
+
+  const rootIntersection = useIntersectionObserver(rootRef, {
+    threshold: [0, 0.5],
+  });
+
   return (
     <NextImage
       {...props}
       alt={props.alt || ""}
       className={clsx(
-        "opacity-0 transition-opacity data-[loaded=true]:opacity-100",
+        "transition duration-700 ease-in-out motion-reduce:transition-none",
+        "translate-y-24 opacity-0",
+        rootIntersection?.isIntersecting &&
+          "data-[loaded=true]:translate-y-0 data-[loaded=true]:opacity-100",
         props.className
       )}
       onLoadingComplete={(img: HTMLImageElement) => {
@@ -21,6 +32,7 @@ export default function Image(props: ImageProps) {
           props.onLoadingComplete(img);
         }
       }}
+      ref={rootRef}
     />
   );
 }
