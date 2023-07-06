d73c29b379560b694871d75c82028db7ae3e5559
Karl Podger <karl.podger@primeordinal.com>
2023-07-06 23:13:22 +0100

    chore: Remove Partytown instance

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import { Partytown } from "@builder.io/partytown/react";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import { GoogleTagManager } from "components/analytics/google-tag-manager";
 
 export const Analytics = () => {
   return (
     <>
-      <Partytown
-        debug={true}
-        forward={["dataLayer.push", "fbq", "klaviyo", "ttq", "va"]}
-      />
-      <GoogleTagManager type="text/partytown" />
+      <GoogleTagManager />
       <VercelAnalytics />
     </>
   );
