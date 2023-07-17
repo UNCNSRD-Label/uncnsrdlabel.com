4ad2a6cd7d661ce3c2128badda337cd6a905603e
Karl Podger <karl.podger@primeordinal.com>
2023-07-17 22:32:29 +0100

    chore: Update Prettier config file to use ESM format

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import("prettier").Options} */
+const config = {
   // pnpm doesn't support plugin autoloading
   // https://github.com/tailwindlabs/prettier-plugin-tailwindcss#installation
   plugins: [require("prettier-plugin-tailwindcss")],
   tailwindFunctions: ["clsx"],
 };
+
+export default config;
