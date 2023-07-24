887f72d43a5a5c382ca3615f6d98d636b707ed82
Karl Podger <karl.podger@primeordinal.com>
2023-07-25 00:23:23 +0100

    chore: Fix Prettier configuration

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Options} */
-const config = {
-  // pnpm doesn't support plugin autoloading
-  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss#installation
-  plugins: [require("prettier-plugin-tailwindcss")],
-  tailwindFunctions: ["clsx"],
-};
-
-export default config;
