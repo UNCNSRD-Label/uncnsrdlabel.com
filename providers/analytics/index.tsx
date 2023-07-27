269c8c58e39786a4f40a08c83b4bf9e1742dfd43
Karl Podger <karl.podger@primeordinal.com>
2023-07-27 23:04:20 +0100

    wip: Analytics implementation

--- providers/analytics/index.tsx
+++ providers/analytics/index.tsx
@@ -4,38 +4,58 @@
 import googleTagManager from "@analytics/google-tag-manager";
 import Analytics from "analytics";
 // import eventValidation from "analytics-plugin-event-validation";
+import { useCart, useProduct, useShop } from "@shopify/hydrogen-react";
+import type { Product } from "@shopify/hydrogen-react/storefront-api-types";
 import { PropsWithChildren } from "react";
+import type { PartialDeep } from "type-fest";
 import { AnalyticsProvider } from "use-analytics";
 import shopify from "./shopify";
 
-/* Initialize analytics & load plugins */
-export const analytics = Analytics({
-  app: process.env.NEXT_PUBLIC_SITE_NAME,
-  debug: true,
-  plugins: [
-    // eventValidation({
-    //   // Namespace of current application
-    //   context: "app",
-    //   // Allowed objects
-    //   objects: [
-    //     "document",
-    //     "sites", // example app:sites_cdConfigured
-    //     "user", // example app:user_signup
-    //     "widget", // example app:widget_created
-    //   ],
-    // }),
-    // googleAnalytics({
-    //   trackingId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID!,
-    // }),
-    googleTagManager({
-      containerId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID!,
-    }),
-    shopify({
-      shopId: "gid://shopify/Shop/69103059266",
-    }),
-  ],
-});
-
 export default function AppAnalyticsProvider({ children }: PropsWithChildren) {
+  const { id: cartId } = useCart();
+
+  const product = useProduct() as PartialDeep<
+    Product,
+    {
+      recurseIntoArrays: true;
+    }
+  >;
+
+  const { storefrontId, countryIsoCode, languageIsoCode } = useShop();
+
+  const locale = new Intl.Locale(`${languageIsoCode}-${countryIsoCode}`);
+
+  /* Initialize analytics & load plugins */
+  const analytics = Analytics({
+    app: process.env.NEXT_PUBLIC_SITE_NAME,
+    debug: true,
+    plugins: [
+      // eventValidation({
+      //   // Namespace of current application
+      //   context: "app",
+      //   // Allowed objects
+      //   objects: [
+      //     "document",
+      //     "sites", // example app:sites_cdConfigured
+      //     "user", // example app:user_signup
+      //     "widget", // example app:widget_created
+      //   ],
+      // }),
+      // googleAnalytics({
+      //   trackingId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID!,
+      // }),
+      googleTagManager({
+        containerId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID!,
+      }),
+      shopify({
+        cartId,
+        locale,
+        product,
+        shopId: `gid://shopify/Shop/${process.env.NEXT_PUBLIC_SHOPIFY_SHOP_ID}`,
+        storefrontId,
+      }),
+    ],
+  });
+
   return <AnalyticsProvider instance={analytics}>{children}</AnalyticsProvider>;
 }
--- providers/analytics/types.ts
+++ providers/analytics/types.ts
@@ -1,51 +1,39 @@
-declare module "@analytics/google-analytics" {
-  import type { AnalyticsPlugin } from "analytics";
+// eslint-disable-next-line no-unused-vars
+export type PluginEventFunction = (options: {
+  config: any;
+  instance: any;
+  payload: {
+    type: string;
+    properties: {
+      title: string;
+      url: string;
+      path: string;
+      hash: string;
+      search: string;
+      width: number;
+      height: number;
+    };
+    options: {
+      [key: string]: any;
+    };
+    userId: string;
+    anonymousId: string;
+    meta: {
+      rid: string;
+      ts: number;
+      hasCallback: boolean;
+    };
+  };
+}) => void;
 
-  export interface GoogleAnalyticsConfig {
-    auth?: string;
-    customScriptSrc?: string;
-    dataLayerName?: string;
-    debug?: boolean;
-    execution?: string;
-    preview?: string;
-    trackingId: string;
-  }
-
+export type PluginEventFunctions = {
   // eslint-disable-next-line no-unused-vars
-  function googleAnalytics(config: GoogleAnalyticsConfig): AnalyticsPlugin;
-
-  export default googleAnalytics;
-}
-
-declare module "@analytics/google-tag-manager" {
-  import type { AnalyticsPlugin } from "analytics";
-
-  export interface GoogleTagManagerConfig {
-    auth?: string;
-    containerId: string;
-    customScriptSrc?: string;
-    dataLayerName?: string;
-    debug?: boolean;
-    execution?: string;
-    preview?: string;
-  }
-
-  // eslint-disable-next-line no-unused-vars
-  function googleTagManager(config: GoogleTagManagerConfig): AnalyticsPlugin;
-
-  export default googleTagManager;
-}
-
-declare module "analytics-plugin-event-validation" {
-  import type { AnalyticsPlugin } from "analytics";
-
-  export interface EventValidationConfig {
-    context: string;
-    objects: string[];
-  }
-
-  // eslint-disable-next-line no-unused-vars
-  function eventValidation(config: EventValidationConfig): AnalyticsPlugin;
-
-  export default eventValidation;
-}
+  [key in
+    | "bootstrap"
+    | "page"
+    | "pageStart"
+    | "pageEnd"
+    | "trackStart"
+    | "trackEnd"
+    | "track:addToCart"]: PluginEventFunction;
+};
