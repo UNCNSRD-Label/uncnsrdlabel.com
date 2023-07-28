0ece4c4428e7c89f29e7ec85e530902e704c4063
Karl Podger <karl.podger@primeordinal.com>
2023-07-28 16:13:30 +0100

    chore: Remove unused supabase provider

--- providers/index.tsx
+++ providers/index.tsx
@@ -3,16 +3,13 @@ import { PropsWithChildren } from "react";
 import AnalyticsProvider from "@/providers/analytics";
 import ReactQueryProvider from "@/providers/react-query";
 import ShopifyProvider from "@/providers/shopify";
-// import SupabaseProvider from "@/providers/supabase";
 
 export default function AppProviders({ children }: PropsWithChildren) {
   return (
-    // <SupabaseProvider>
     <ReactQueryProvider>
       <ShopifyProvider>
         <AnalyticsProvider>{children}</AnalyticsProvider>
       </ShopifyProvider>
     </ReactQueryProvider>
-    // </SupabaseProvider>
   );
 }
