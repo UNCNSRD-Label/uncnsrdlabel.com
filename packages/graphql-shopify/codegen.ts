e3dec046976d51761ee2e983db3c0843ed8982a5
Turbobot <turbobot@vercel.com>
2023-07-30 17:34:37 +0100

    feat(create-turbo): apply official-starter transform

--- packages/graphql-shopify/codegen.ts
+++ packages/graphql-shopify/codegen.ts
@@ -1,18 +1,20 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import { storefrontApiCustomScalars } from '@shopify/hydrogen-react';
 
 const config: CodegenConfig = {
   ignoreNoDocuments: true, // for better experience with the watcher
   overwrite: true,
-  schema: {
-    // [`https://${process.env.NEXT_PUBLIC_HYGRAPH_REGION}.cdn.hygraph.com/content/${process.env.NEXT_PUBLIC_HYGRAPH_PROJECT_ID}/master`]:
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //   },
-    schema: '@shopify/hydrogen-react/storefront.schema.json',
-  },
+  // schema: {
+  //   // [`https://${process.env.NEXT_PUBLIC_HYGRAPH_REGION}.cdn.hygraph.com/content/${process.env.NEXT_PUBLIC_HYGRAPH_PROJECT_ID}/master`]:
+  //   //   {
+  //   //     headers: {
+  //   //       Authorization: `Bearer ${process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN}`,
+  //   //       "Content-Type": "application/json",
+  //   //     },
+  //   //   },
+  //   schema: '@shopify/hydrogen-react/storefront.schema.json',
+  // },
+  schema: require.resolve('@shopify/hydrogen-react/storefront.schema.json'),
   documents: [
     "./src/**/*.{graphql,ts,tsx}",
     "../../apps/app/app/**/*.shopify.graphql",
@@ -22,7 +24,11 @@ const config: CodegenConfig = {
   ],
   emitLegacyCommonJSImports: false,
   generates: {
-    "./src/": {
+    "./generated/": {
+      config: {
+        // defines the custom scalars used in the Storefront API
+        scalars: storefrontApiCustomScalars,
+      },
       preset: "client",
     },
   },
