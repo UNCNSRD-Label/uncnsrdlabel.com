589387bb0f2282fc723b31641af0cc6305a2c9ea
Karl Podger <karl.podger@primeordinal.com>
2023-08-08 12:00:01 +0100

    wip: Reinstate site code check and redirect

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,38 +1,57 @@
 import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-const headers = { "accept-language": "en-US,en;q=0.5" };
-const languages = new Negotiator({ headers }).languages();
-const locales = ["en-US", "nl-NL", "nl"];
 const defaultLocale = "en-US";
+const headers = { "accept-language": `${defaultLocale},en;q=0.5` };
+const languages = new Negotiator({ headers }).languages();
+const locales = (
+  process.env.NEXT_PUBLIC_SUPPORTED_LOCALES ?? defaultLocale
+).split(",");
 
 const savedCode = "arizona";
 
 const target = "https://holding.uncnsrdlabel.com";
 
-const getLocale = (request) => match(languages, locales, defaultLocale) ?? 'en-US';
- 
-export function middleware(request) {
+const getLocale = () => match(languages, locales, defaultLocale);
+
+export function middleware(request: NextRequest) {
+  const suppliedCode = request.nextUrl.searchParams.get("code");
+
+  const response = NextResponse.next();
+
+  if (suppliedCode === savedCode) {
+    response.cookies.set("preview", "true");
+  }
+
+  if (
+    suppliedCode !== savedCode &&
+    request.cookies.get("preview")?.value !== "true"
+  ) {
+    return NextResponse.redirect(target);
+  }
+
   // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname
-  console.log(pathname);
+  const pathname = request.nextUrl.pathname;
+
   const pathnameIsMissingLocale = locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
- 
+    (locale) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  );
+
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
- 
+    const locale = getLocale();
+
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
     return NextResponse.redirect(
-      new URL(`/${locale}/${pathname}`, request.url)
-    )
+      new URL(`/${locale}/${pathname}`, request.url),
+    );
   }
-}
 
+  return response;
+}
 
 // Stop Middleware running on static files and public folder
 export const config = {
