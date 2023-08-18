c055dfe352bf92d3193133fafda326ce01ed337e
Karl Podger <karl.podger@primeordinal.com>
2023-08-18 11:11:39 +0100

    chore: Add common naming convention to i18n utilities

--- packages/lib/src/i18n.ts
+++ packages/lib/src/i18n.ts
@@ -1,9 +1,13 @@
 import { IETFLanguageTag } from "@uncnsrdlabel/types";
 
-export const localeTagToIETFLanguageTag = (localeTag: Intl.Locale) => localeTag?.baseName as IETFLanguageTag
+export const getLocaleTagFromIETFLanguageTag = (
+  tag = process.env.NEXT_PUBLIC_DEFAULT_LOCALE! as Intl.BCP47LanguageTag,
+) => new Intl.Locale(tag);
+
+export const getIETFLanguageTagFromlocaleTag = (localeTag: Intl.Locale) => localeTag?.baseName as IETFLanguageTag
 
 export const defaultLocale = new Intl.Locale(process.env.NEXT_PUBLIC_DEFAULT_LOCALE ?? "en-AU");
 
 export const locales = (
-  process.env.NEXT_PUBLIC_SUPPORTED_LOCALES ?? localeTagToIETFLanguageTag(defaultLocale)
+  process.env.NEXT_PUBLIC_SUPPORTED_LOCALES ?? getIETFLanguageTagFromlocaleTag(defaultLocale)
 ).split(",").map(locale => new Intl.Locale(locale));
