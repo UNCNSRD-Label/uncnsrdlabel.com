d0c7f20bcdfcfa4dd97bfa7239bedd032101655d
Karl Podger <karl.podger@primeordinal.com>
2023-08-21 18:04:49 +0100

    wip: Add code match functionality to middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -11,7 +11,44 @@ const locales = (
 const getLocale = (languages: string[]) =>
   match(languages, locales, defaultLocale);
 
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
+
 export function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+
+  const accessCodeActual = process.env.NEXT_PUBLIC_PREVIEW_ACCESS_CODE!;
+
+  const accessCodeReceived = url.searchParams.get("code");
+
+  const match = accessCodeReceived === accessCodeActual;
+
+  const previewCookie = request.cookies.get("preview")?.value;
+
+  if (match === true) {
+    url.searchParams.delete("code");
+
+    const response = NextResponse.redirect(url);
+
+    response.cookies.set("preview", "true");
+
+    return response;
+  } else {
+    if (previewCookie !== "true") {
+      return NextResponse.redirect("https://access.uncnsrdlabel.com");
+    }
+  }
+
   const detectedLanguage =
     request.headers
       .get("accept-language")
@@ -19,16 +56,17 @@ export function middleware(request: NextRequest) {
       .split("-")?.[0]
       .toLowerCase() || "en";
 
-  const detectedCountry = request.geo?.country ?? defaultLocale.split("-")?.[1] ?? "AU";
+  const detectedCountry =
+    request.geo?.country ?? defaultLocale.split("-")?.[1] ?? "AU";
 
-  const headers = { "accept-language": `${detectedLanguage}-${detectedCountry},en;q=0.5` };
+  const headers = {
+    "accept-language": `${detectedLanguage}-${detectedCountry},en;q=0.5`,
+  };
 
   const languages = new Negotiator({ headers }).languages();
 
-  const response = NextResponse.next();
-
   // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname;
+  const pathname = url.pathname;
 
   const pathnameIsMissingLocale = locales.every(
     (locale) =>
@@ -39,28 +77,11 @@ export function middleware(request: NextRequest) {
   if (pathnameIsMissingLocale) {
     const locale = getLocale(languages);
 
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
+    // e.g. incoming request is /products, new URL is now /en-AU/products
     return NextResponse.redirect(
       new URL(`/${locale}/${pathname}`, request.url),
     );
   }
 
-  return response;
+  return NextResponse.next();
 }
-
-// Stop Middleware running on static files and public folder
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     * - public folder
-     */
-    "/((?!static|.*\\..*|_next|api|favicon.ico).*)",
-    "/",
-  ],
-};
