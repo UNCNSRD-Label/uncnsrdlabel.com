0ff88f2ee5f6eccc33d3db5bc6df09f28a5a5124
Karl Podger <karl.podger@primeordinal.com>
2023-08-25 23:45:28 +0100

    wip: Package configuration

--- packages/graphql-shopify-storefront/tsup.config.ts
+++ packages/graphql-shopify-storefront/tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig, Options } from "tsup";
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ["src/**/*.ts", "src/**/*.tsx"],
+  entry: ["src/**/*.ts"],
   format: ["esm"],
   dts: true,
   minify: true,
   clean: true,
-  external: ["react"],
   ...options,
 }));
