07cc3742ee5464a8d0b44cb1e7d42d2f64b54d7b
Karl Podger <karl.podger@primeordinal.com>
2023-08-29 22:09:46 +0100

    chore: Add .ts and .tsx files to tsup imports

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  banner: {
-    js: "'use client'",
-  },
+  treeshake: true,
+  splitting: true,
+  entry: ["src/**/*.{ts,tsx}"],
+  format: ["cjs", "esm"],
+  dts: true,
+  minify: false,
+  clean: true,
   ...options,
 }));
