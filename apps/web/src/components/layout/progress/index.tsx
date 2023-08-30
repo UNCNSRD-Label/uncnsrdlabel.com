9d4fe87f9ca7d9053198264400eba49cbc9aef1f
Karl Podger <karl.podger@primeordinal.com>
2023-08-31 00:33:22 +0100

    chore: Remove unused progress CSS module

--- apps/web/src/components/layout/progress/index.tsx
+++ apps/web/src/components/layout/progress/index.tsx
@@ -1,7 +1,3 @@
 export function Progress() {
   return <div className="supports-not-[animation-timeline:view()]:hidden [animation-timeline:--page-scroll] animate-grow-progress bg-hotPink h-0.5 fixed inset-x-0 top-0 z-50 origin-left" />;
 }
-
-// animation: linear growProgress forwards;
-// animation-timeline: --page-scroll;
-// transform-origin: 0 50%;
