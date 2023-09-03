7d4882ec6292e400cd425577d524b270e0f2cc4f
Karl Podger <karl.podger@primeordinal.com>
2023-09-03 12:03:23 +0100

    fix: Rework scroll progress indicator in Tailwind

--- apps/web/src/components/layout/progress/index.tsx
+++ apps/web/src/components/layout/progress/index.tsx
@@ -1,3 +1,3 @@
 export function Progress() {
-  return <div className="supports-not-[animation-timeline:view()]:hidden [animation-timeline:--page-scroll] animate-grow-progress bg-hotPink h-0.5 fixed inset-x-0 top-0 z-50 origin-left" />;
+  return <i className="supports-not-[animation-timeline:view()]:hidden [animation-timeline:_scroll()] animate-grow-progress bg-hotPink h-0.5 fixed inset-x-0 top-0 z-50 origin-left" />;
 }
