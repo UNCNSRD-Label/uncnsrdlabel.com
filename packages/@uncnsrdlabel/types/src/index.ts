3bec8cbaa70c75af7d87a8d1b3fb28f373888be9
Karl Podger <karl.podger@primeordinal.com>
2023-09-06 18:11:38 +0100

    chore: Export Next layout and page types

--- packages/@uncnsrdlabel/types/src/index.ts
+++ packages/@uncnsrdlabel/types/src/index.ts
@@ -1,4 +1,5 @@
 export type * from "./i18n";
 export type * from "./klaviyo";
+export type * from "./next";
 export type * from "./ts-reset";
 
---
+++ packages/@uncnsrdlabel/types/src/next.ts
@@ -0,0 +1,8 @@
+
+export type LayoutProps = {
+  params: { handle: string; lang: Intl.BCP47LanguageTag };
+};
+
+export type PageProps = {
+  params: { handle: string; lang: Intl.BCP47LanguageTag };
+};
