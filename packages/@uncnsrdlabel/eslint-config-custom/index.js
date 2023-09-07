ff635996e7f6798eb29ce539507888276e1b2218
Karl Podger <karl.podger@primeordinal.com>
2023-09-08 00:26:29 +0100

    fix: ESLint configuration

--- packages/@uncnsrdlabel/eslint-config-custom/index.js
+++ packages/@uncnsrdlabel/eslint-config-custom/index.js
@@ -1,11 +1,29 @@
 export default {
   extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
+  plugins: ["unicorn"],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "no-unused-vars": [
+      "error",
+      {
+        args: "after-used",
+        caughtErrors: "none",
+        ignoreRestSiblings: true,
+        vars: "all",
+      },
+    ],
+    "prefer-const": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "unicorn/filename-case": [
+      "error",
+      {
+        case: "kebabCase",
+      },
+    ],
   },
 };
