28ca05df86aa646303263bfe2f177cd3ea4a85d9
Karl Podger <karl.podger@primeordinal.com>
2023-11-08 18:09:58 +0000

    chore: Remove access redirect

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -27,30 +27,6 @@ export const config = {
 export function middleware(request: NextRequest) {
   const url = request.nextUrl;
 
-  const accessCodeActual = process.env.NEXT_PUBLIC_PREVIEW_ACCESS_CODE!;
-
-  const accessCodeReceived = url.searchParams.get("code");
-
-  const match = accessCodeReceived === accessCodeActual;
-
-  const previewCookie = request.cookies.get("preview")?.value;
-
-  if (match === true) {
-    url.searchParams.delete("code");
-
-    const response = NextResponse.redirect(url);
-
-    response.cookies.set("preview", "true");
-
-    return response;
-  } else {
-    if (previewCookie !== "true") {
-      return NextResponse.redirect(
-        `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_SITE_DOMAIN_ACCESS}`,
-      );
-    }
-  }
-
   const detectedLanguage =
     request.headers
       .get("accept-language")
