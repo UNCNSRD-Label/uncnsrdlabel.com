2f59d559d4e078b61c49ead86423755393a67352
Karl Podger <karl.podger@primeordinal.com>
2023-11-08 12:12:05 +0000

    fix: Render Shopify fields using Markdown to including newlines, etc.

--- apps/web/src/components/prose.tsx
+++ apps/web/src/components/prose.tsx
@@ -1,21 +1,33 @@
 import { cn } from "@uncnsrdlabel/lib";
-import type { FunctionComponent } from "react";
-import { HTMLProps } from "react";
+import { HTMLProps, type FunctionComponent } from "react";
+import Markdown from "react-markdown";
+import { type RequireExactlyOne } from "type-fest";
 
-export type TextProps = HTMLProps<HTMLElement> & {
+export type ProseProps = HTMLProps<HTMLElement> & {
   html: string;
+  markdown: string;
 };
 
-export const Prose: FunctionComponent<TextProps> = ({
-  className,
-  html,
-  style,
-}) => {
-  return (
-    <div
-      className={cn("prose", className)}
-      dangerouslySetInnerHTML={{ __html: html }}
-      style={style}
-    />
-  );
+export const Prose: FunctionComponent<
+  RequireExactlyOne<ProseProps, "html" | "markdown">
+> = ({ className, html, markdown, style }) => {
+  if (markdown) {
+    return (
+      <div className={cn("prose", className)} style={style}>
+        <Markdown>{markdown}</Markdown>
+      </div>
+    );
+  }
+
+  if (html) {
+    return (
+      <div
+        className={cn("prose", className)}
+        dangerouslySetInnerHTML={{ __html: html }}
+        style={style}
+      />
+    );
+  }
+
+  return null;
 };
