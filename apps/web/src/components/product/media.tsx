947d454fc1270d26fc92f59769a3c58d8246b708
Karl Podger <karl.podger@primeordinal.com>
2023-11-12 12:24:40 +0000

    fix: Remove black background from PDP images so overlaying text is visible during loading

--- apps/web/src/components/product/media.tsx
+++ apps/web/src/components/product/media.tsx
@@ -23,7 +23,7 @@ export function ProductMedia({
     "lg:aspect-2/3 min-h-[100dvh] lg:h-[100dvh] overflow-y-clip relative snap-start w-[100dvw] lg:w-auto";
 
   const thumbnailClassName =
-    "bg-black pointer-events-auto relative my-auto aspect-square w-full overflow-hidden rounded-full shadow";
+    "pointer-events-auto relative my-auto aspect-square w-full overflow-hidden rounded-full shadow";
 
   const product = getFragmentData(
     productDetailsFragment,
