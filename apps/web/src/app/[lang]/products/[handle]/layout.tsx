5e255790ca21de055a33c90956370aa4022039da
Karl Podger <karl.podger@primeordinal.com>
2023-11-12 16:09:42 +0000

    fix: Remove PageTransition instances to allow SSR

--- apps/web/src/app/[lang]/products/[handle]/layout.tsx
+++ apps/web/src/app/[lang]/products/[handle]/layout.tsx
@@ -2,7 +2,6 @@ import { Logo } from "@/components/layout/logo/index";
 import { NavbarContent } from "@/components/layout/navbar/content";
 import { Navbar } from "@/components/layout/navbar/index";
 import { LoadingDots } from "@/components/loading/dots";
-import { PageTransition } from "@/components/page-transition";
 import { Suspense } from "react";
 
 export default function SearchLayout({
@@ -16,7 +15,7 @@ export default function SearchLayout({
         <NavbarContent />
       </Navbar>
       <Suspense fallback={<LoadingDots />}>
-        <PageTransition>{children}</PageTransition>
+        {children}
         <Logo className="bottom-20" />
       </Suspense>
     </>
