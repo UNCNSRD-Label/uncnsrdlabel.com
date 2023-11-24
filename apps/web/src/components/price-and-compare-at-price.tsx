a43a0304e1ce10471babcb3a9579996b6038b821
Karl Podger <karl.podger@primeordinal.com>
2023-11-24 01:49:04 +0000

    chore: Add compareAtPrice to PLP

---
+++ apps/web/src/components/price-and-compare-at-price.tsx
@@ -0,0 +1,64 @@
+import { Price } from "@/components/price";
+import {
+  getFragmentData,
+  productBasicFragment,
+  productDetailsFragment,
+} from "@uncnsrdlabel/graphql-shopify-storefront";
+import { FragmentType } from "@uncnsrdlabel/graphql-shopify-storefront/client";
+import { cn } from "@uncnsrdlabel/lib";
+
+export const PriceAndCompareAtPrice = ({
+  className,
+  currencyCodeClassName,
+  productBasicFragmentRef,
+  productDetailsFragmentRef,
+}: {
+  className?: string;
+  currencyCodeClassName?: string;
+  productBasicFragmentRef?: FragmentType<typeof productBasicFragment>;
+  productDetailsFragmentRef?: FragmentType<typeof productDetailsFragment>;
+} & React.ComponentProps<"div">) => {
+  let product;
+
+  if (productBasicFragmentRef) {
+    product = getFragmentData(productBasicFragment, productBasicFragmentRef);
+  }
+
+  if (productDetailsFragmentRef) {
+    product = getFragmentData(
+      productDetailsFragment,
+      productDetailsFragmentRef,
+    );
+  }
+
+  if (!product) {
+    console.error("No product data found");
+    return null
+  };
+
+  return (
+    <div
+      className={cn("grid grid-flow-col justify-start gap-4", className)}
+    >
+      <Price
+        className={cn("font-semibold", {
+          "text-red-600": !!product.compareAtPriceRange?.minVariantPrice,
+        })}
+        amount={product.priceRange.minVariantPrice.amount}
+        currencyCode={product.priceRange.minVariantPrice.currencyCode}
+        currencyCodeClassName={currencyCodeClassName}
+      />
+
+      {product.compareAtPriceRange?.minVariantPrice && (
+        <Price
+          className="line-through decoration-red-600"
+          amount={product.compareAtPriceRange.minVariantPrice.amount}
+          currencyCode={
+            product.compareAtPriceRange.minVariantPrice.currencyCode
+          }
+          currencyCodeClassName={currencyCodeClassName}
+        />
+      )}
+    </div>
+  );
+};
