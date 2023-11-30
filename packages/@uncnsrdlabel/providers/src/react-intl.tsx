5643469bdaf76eaaf46f2a48624a6464bb88fb0d
Karl Podger <karl.podger@primeordinal.com>
2023-11-30 02:07:57 +0000

    wip: Pass argument to provider

--- packages/@uncnsrdlabel/providers/src/react-intl.tsx
+++ packages/@uncnsrdlabel/providers/src/react-intl.tsx
@@ -1,25 +1,18 @@
 "use client";
 
-import {
-  getLocalizationHandler,
-} from "@uncnsrdlabel/graphql-shopify-storefront/server";
 import { PropsWithChildren } from "react";
 import { IntlProvider, type ResolvedIntlConfig } from "react-intl";
 
-export async function AppIntlProvider({
+export function AppIntlProvider({
   children,
+  defaultLocale,
   lang,
   messages,
 }: PropsWithChildren<{
+  defaultLocale: Intl.BCP47LanguageTag;
   lang: Intl.BCP47LanguageTag;
   messages: ResolvedIntlConfig["messages"];
 }>) {
-  const localization = await getLocalizationHandler({
-    lang,
-  });
-
-  const defaultLocale = localization.country.isoCode;
-
   return (
     <IntlProvider
       messages={messages}
