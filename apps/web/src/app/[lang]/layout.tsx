455a76b93c725fa503efba7f0abcd78e7847f0d8
Karl Podger <karl.podger@primeordinal.com>
2023-12-10 21:08:08 +0000

    fix: Add languages to handling

--- apps/web/src/app/[lang]/layout.tsx
+++ apps/web/src/app/[lang]/layout.tsx
@@ -9,10 +9,13 @@ import { getBaseMetadata } from "@/lib/metadata";
 import { state$ } from "@/lib/store";
 import { themeColors } from "@/lib/tailwind";
 import { type LayoutProps } from "@/types/next";
-import { type CountryCode, type LanguageCode } from "@shopify/hydrogen/storefront-api-types";
-import { getLocalizationAvailableBCP47LanguageTagsHandler, getLocalizationDetailsHandler } from "@uncnsrdlabel/graphql-shopify-storefront";
 import {
-  DEFAULT_BCP47_LANGUAGE_TAGS,
+  type CountryCode,
+  type LanguageCode,
+} from "@shopify/hydrogen/storefront-api-types";
+import { getLocalizationDetailsHandler } from "@uncnsrdlabel/graphql-shopify-storefront";
+import {
+  PRE_GENERATED_BCP47_LANGUAGE_TAGS,
   cn,
   getIETFLanguageTagFromlocaleTag,
   getLocaleObjectFromIETFLanguageTag,
@@ -91,7 +94,12 @@ const montserrat = Montserrat({
 });
 
 export async function generateStaticParams() {
-  const localization = await getLocalizationAvailableBCP47LanguageTagsHandler();
+  const localization = await getLocalizationDetailsHandler({});
+
+  const languageCodes = localization.availableLanguages.map(
+    (availableLanguage) =>
+      availableLanguage.isoCode.toLocaleLowerCase() as LanguageCode,
+  );
 
   const shopifyBCP47LanguageTags: Intl.BCP47LanguageTag[] =
     localization.availableCountries.flatMap((availableCountry) =>
@@ -103,13 +111,14 @@ export async function generateStaticParams() {
       ),
     );
 
-  const supportedDefaultBCP47LanguageTags = intersection(
-    DEFAULT_BCP47_LANGUAGE_TAGS,
+  // To prevent extremely large builds, limit pre-generated BCP47 language tags to the intersection of the ones the Shopify store supports and the hard-coded list
+  const preGeneratedBCP47LanguageTags = intersection(
+    PRE_GENERATED_BCP47_LANGUAGE_TAGS,
     shopifyBCP47LanguageTags,
   );
 
-  return supportedDefaultBCP47LanguageTags.map((BCP47LanguageTag) => ({
-    lang: BCP47LanguageTag,
+  return [...languageCodes, ...preGeneratedBCP47LanguageTags].map((lang) => ({
+    lang,
   }));
 }
 
@@ -119,12 +128,14 @@ export default async function RootLayout({
 }: PropsWithChildren<LayoutProps>) {
   const showBanner = false;
 
-  const localization = await getLocalizationDetailsHandler({ lang });
+  if (lang) {
+    const localization = await getLocalizationDetailsHandler({ lang });
 
-  state$.country.set(lang.split("-")[1] as CountryCode);
-  state$.lang.set(lang);
-  state$.language.set(lang.split("-")[0] as LanguageCode);
-  state$.localization.set(localization);
+    state$.country.set(lang.split("-")[1] as CountryCode);
+    state$.lang.set(lang);
+    state$.language.set(lang.split("-")[0] as LanguageCode);
+    state$.localization.set(localization);
+  }
 
   return (
     <html
