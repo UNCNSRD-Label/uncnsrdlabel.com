978a46c260197912e407554c77c038aa3270e85b
Karl Podger <karl.podger@primeordinal.com>
2023-12-12 23:08:26 +0000

    fix: Refine styling of Banner message

--- apps/web/src/components/layout/banner.tsx
+++ apps/web/src/components/layout/banner.tsx
@@ -23,36 +23,38 @@ export const Banner = ({ className }: { className?: string }) => {
   return (
     <article
       className={cn(
-        "bg-hotPink text-light grid snap-start place-content-center p-7 uppercase",
+        "bg-hotPink text-light grid snap-start place-content-center p-6 text-xs uppercase sm:text-base",
         className,
       )}
     >
-      {discountNodes?.edges
-        ?.map((edge) => edge.node)
-        ?.map((discountNodeFragmentRef) => {
-          const discountNode = getFragmentData(
-            discountNodeFragment,
-            discountNodeFragmentRef,
-          );
+      {
+        discountNodes?.edges
+          ?.map((edge) => edge.node)
+          ?.map((discountNodeFragmentRef) => {
+            const discountNode = getFragmentData(
+              discountNodeFragment,
+              discountNodeFragmentRef,
+            );
 
-          switch (discountNode.discount.__typename) {
-            case "DiscountCodeBasic":
-              return (
-                <details className="mt-4" data-type="DiscountCodeBasic">
-                  <summary className="text-xs sm:text-base">{discountNode.discount.title} - {discountNode.discount.shortSummary}</summary>
-                  {/* <span>{discountNode.discount.summary}</span> */}
-                </details>
-              );
+            switch (discountNode.discount.__typename) {
+              case "DiscountCodeBasic":
+                return (
+                  <span data-type="DiscountCodeBasic">
+                    {discountNode.discount.title} -{" "}
+                    {discountNode.discount.shortSummary}
+                  </span>
+                );
 
-            case "DiscountAutomaticBasic":
-              return (
-                <details className="mt-4" data-type="DiscountAutomaticBasic">
-                  <summary className="text-xs sm:text-base">{discountNode.discount.title} - {discountNode.discount.shortSummary}</summary>
-                  {/* <span>{discountNode.discount.summary}</span> */}
-                </details>
-              );
-          }
-        })?.[0]}
+              case "DiscountAutomaticBasic":
+                return (
+                  <span data-type="DiscountAutomaticBasic">
+                    {discountNode.discount.title} -{" "}
+                    {discountNode.discount.shortSummary}
+                  </span>
+                );
+            }
+          })?.[0]
+      }
     </article>
   );
 };
