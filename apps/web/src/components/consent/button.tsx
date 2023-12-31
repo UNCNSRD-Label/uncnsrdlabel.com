05cd4865def3a5506376b65f960791faa6d668d8
Karl Podger <karl.podger@primeordinal.com>
2023-12-31 19:01:52 +0000

    fix: Set onClick property of consent Button component as optional

--- apps/web/src/components/consent/button.tsx
+++ apps/web/src/components/consent/button.tsx
@@ -1,7 +1,7 @@
 import { getIntl } from "@/lib/i18n";
 import { state$ } from "@/lib/store";
 
-export async function Button({ className, onClick }: { className?: string; onClick: () => void }) {
+export async function Button({ className, onClick }: { className?: string; onClick?: () => void }) {
   const lang = state$.lang.get();
 
   const intl = await getIntl(lang, "component.ConsentDialog");
