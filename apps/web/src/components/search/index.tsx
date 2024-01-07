9243119bf860ea2b77f918599101186fb79961d1
Karl Podger <karl.podger@primeordinal.com>
2024-01-07 22:16:16 +0000

    fix: Reinstate missing suspense boundaries to prevent pages being deopted into client-side rendering

--- apps/web/src/components/search/index.tsx
+++ apps/web/src/components/search/index.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { useState } from "react";
-
 import { SearchButton } from "@/components/search/button";
 import { SearchForm } from "@/components/search/form";
+import { Suspense, useState } from "react";
 
 export function Search() {
   const [searchIsOpen, setSearchIsOpen] = useState(false);
 
   return (
     <div className="relative grid w-full items-center" tabIndex={-1}>
-      <SearchForm isOpen={searchIsOpen} setSearchIsOpen={setSearchIsOpen} />
+      <Suspense fallback={null}>
+        <SearchForm isOpen={searchIsOpen} setSearchIsOpen={setSearchIsOpen} />
+      </Suspense>
       <SearchButton isOpen={searchIsOpen} />
     </div>
   );
