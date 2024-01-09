0e98c319c3785dd745f4063f7d9711e95881072d
Karl Podger <karl.podger@primeordinal.com>
2024-01-09 12:56:04 +0000

    fix: Set return type of getDictionary to Promise<ResolvedIntlConfig["messages"]>

--- apps/web/src/lib/dictionary.ts
+++ apps/web/src/lib/dictionary.ts
@@ -6,7 +6,7 @@ import merge from "deepmerge";
 import { deepKeys, getProperty } from 'dot-prop';
 import { type ResolvedIntlConfig } from "react-intl";
 
-export const getDictionary = async ({ localization, namespace }: { localization: ResultOf<typeof getLocalizationDetailsQuery>['localization']; namespace?: string }) => {
+export const getDictionary = async ({ localization, namespace }: { localization: ResultOf<typeof getLocalizationDetailsQuery>['localization']; namespace?: string }): Promise<ResolvedIntlConfig["messages"]> => {
   let languageFallback = {}
   let languageGeneric = {}
   let languageLocalised = {}
@@ -44,9 +44,9 @@ export const getDictionary = async ({ localization, namespace }: { localization:
     return allMessagesFlattened
   }
 
-  const namespaceMessages = getProperty(allMessages, namespace) as ResolvedIntlConfig["messages"];
+  const namespaceMessages = getProperty(allMessages, namespace);
 
-  const namespaceMessagesFlattened = deepKeys(namespaceMessages);
+  const namespaceMessagesFlattened = deepKeys(namespaceMessages) as unknown as ResolvedIntlConfig["messages"];
 
   return namespaceMessagesFlattened
 };
