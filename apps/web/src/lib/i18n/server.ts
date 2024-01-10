7f0d7c758ee6f8ec208e65a363dbc9283b82da89
Karl Podger <karl.podger@primeordinal.com>
2024-01-10 11:26:14 +0000

    fix: Add useSelector around state$ instances

--- apps/web/src/lib/i18n/server.ts
+++ apps/web/src/lib/i18n/server.ts
@@ -1,11 +1,13 @@
 import { state$ } from "@/lib/store";
 import { createIntl, type IntlShape } from "@formatjs/intl";
+import { useSelector } from "@legendapp/state/react";
+import { type ResolvedIntlConfig } from "react-intl";
 
 export type GetIntlFn = (_tag: string | undefined, _namespace: string) => Promise<IntlShape<string>>
 
 export function getIntl() {
-  const locale = state$.lang.get();
-  const messages = state$.messages.get();
+  const locale = useSelector<Intl.BCP47LanguageTag>(() => state$.lang.get());
+  const messages = useSelector<ResolvedIntlConfig["messages"]>(() => state$.messages.get());
 
   return createIntl({
     locale,
