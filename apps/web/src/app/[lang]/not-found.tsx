787d5ec67f0d5e6bceb61e1237cedc48b9c850c6
Karl Podger <karl.podger@primeordinal.com>
2024-01-15 22:53:11 +0000

    fix: Use i18n for not-found page

--- apps/web/src/app/[lang]/not-found.tsx
+++ apps/web/src/app/[lang]/not-found.tsx
@@ -1,12 +1,34 @@
 import { Boundary } from "@/components/boundary";
+import { getDictionary } from "@/lib/dictionary";
+import { type PageProps } from "@/types/next";
+import { createIntl } from "@formatjs/intl";
+import { Link } from "@uncnsrdlabel/components/atoms/link";
+import { type ResolvedIntlConfig } from "react-intl";
 
-export default function NotFound() {
+const handle = "not-found";
+
+export default async function NotFound({ params: { lang } }: PageProps) {
+  const messages: ResolvedIntlConfig["messages"] = await getDictionary({
+    lang,
+  });
+
+  const intl = createIntl({
+    locale: lang,
+    messages,
+  });
+
   return (
     <Boundary labels={["not-found.tsx"]} color="pink">
       <div className="text-vercel-pink space-y-4">
-        <h2 className="text-lg font-bold">Not Found</h2>
-
-        <p className="text-sm">Could not find requested resource</p>
+        <h2 className="text-lg font-bold">{intl.formatMessage({ id: `page.${handle}.title` })}</h2>
+        <span className="text-sm">{intl.formatMessage({ id: `page.${handle}.message` })}</span>
+        <Link
+          aria-label={intl.formatMessage({ id: `page.${handle}.button` })}
+          className="btn btn-outline btn-primary btn-base absolute z-20 justify-self-center whitespace-nowrap uppercase"
+          href="/collections/all"
+        >
+          {intl.formatMessage({ id: `page.${handle}.button` })}
+        </Link>
       </div>
     </Boundary>
   );
