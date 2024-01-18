2040a223acb1bca0f73a04f6aef5aba626e615df
Karl Podger <karl.podger@primeordinal.com>
2024-01-18 00:13:00 +0000

    fix: Unable to load dictionary messages for client-side error page

--- apps/web/src/app/[lang]/error.tsx
+++ apps/web/src/app/[lang]/error.tsx
@@ -1,30 +1,16 @@
 "use client";
 
 import { LogotypeIcon } from "@/components/icons/logotype";
-// import { state$ } from "@/lib/store";
-// import { createIntl } from "@formatjs/intl";
-// import { useSelector } from "@legendapp/state/react";
 import { Button } from "@uncnsrdlabel/components/ui/button";
-// import { use } from "react";
-// import { type ResolvedIntlConfig } from "react-intl";
 
-export default function Error({ reset }: { reset: () => void }) {
-  // const messages = use<ResolvedIntlConfig["messages"]>(getDictionary);
-
-  // const locale = useSelector<string>(() => state$.lang.get());
-
-  // const intl = createIntl({
-  //   locale,
-  //   messages,
-  // });
-
+export default function Error({ reset }: { reset: () => void; }) {
   return (
     <div className="mx-4 my-16 grid h-96 place-content-center place-items-center gap-8">
       <LogotypeIcon className="w-96 fill-inherit" />
-      {/* <h2>{intl.formatMessage({ id: "page.error.title" })}</h2> */}
-      {/* <p>{intl.formatMessage({ id: "page.error.message" })}</p> */}
+      <h2>Not found</h2>
+      <p>Could not find requested resource</p>
       <Button variant="default" size="lg" onClick={() => reset()}>
-        {/* {intl.formatMessage({ id: "page.error.button" })} */}
+        Try again
       </Button>
     </div>
   );
