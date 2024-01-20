131da73214e55d7febdb9762812439ce30300200
Karl Podger <karl.podger@primeordinal.com>
2024-01-20 16:07:21 +0000

    fix: Remove unused code from AddToCart

--- apps/web/src/components/cart/add-to-cart.tsx
+++ apps/web/src/components/cart/add-to-cart.tsx
@@ -6,22 +6,22 @@ import { createIntl } from "@formatjs/intl";
 import { CheckIcon, ClockIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { useSelector } from "@legendapp/state/react";
 import {
-    type CartLineInput,
-    type ProductOption,
-    type ProductVariant,
+  type CartLineInput,
+  type ProductOption,
+  type ProductVariant,
 } from "@shopify/hydrogen/storefront-api-types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button, type ButtonProps } from "@uncnsrdlabel/components/ui/button";
 import {
-    addToCartMutation,
-    cartFragment,
-    createCartMutation,
-    getCartQuery,
-    getFragmentData,
-    getQueryKey,
-    getShopifyGraphQL,
-    type AddToCartMutationVariables,
-    type CreateCartMutationVariables,
+  addToCartMutation,
+  cartFragment,
+  createCartMutation,
+  getCartQuery,
+  getFragmentData,
+  getQueryKey,
+  getShopifyGraphQL,
+  type AddToCartMutationVariables,
+  type CreateCartMutationVariables,
 } from "@uncnsrdlabel/graphql-shopify-storefront";
 import { cn, getLangProperties } from "@uncnsrdlabel/lib";
 import { useSearchParams } from "next/navigation";
@@ -74,10 +74,6 @@ function SubmitButton({
   const createCartMutationFn = (variables: CreateCartMutationVariables) =>
     getShopifyGraphQL(createCartMutation, variables);
 
-  type AddToCartContext = { id: number };
-
-  type CreateCartContext = { id: number };
-
   const {
     // data: dataAddToCart = null,
     isPending: isPendingAddToCart,
@@ -86,25 +82,7 @@ function SubmitButton({
   } = useMutation({
     mutationFn: addToCartMutationFn,
     mutationKey: ["addToCart", cartId],
-    onError: (error, variables, context?: AddToCartContext) => {
-      // An error happened!
-      // console.log("onError", { error, variables, context });
-      // console.log(`rolling back optimistic update with id ${context?.id}`);
-    },
-    onMutate: (variables): AddToCartContext => {
-      // A mutation is about to happen!
-      // console.log("onMutate", { variables });
-
-      // Optionally return a context containing data to use when for example rolling back
-      return { id: 1 };
-    },
-    onSettled: (data, error, variables, context) => {
-      // Error or success... doesn't matter!
-      // console.log("onSettled", { data, error, variables, context });
-    },
-    onSuccess: (data, variables, context) => {
-      // console.log("onSuccess", { data, variables, context });
-
+    onSuccess: (data) => {
       const queryKey = getQueryKey(getCartQuery, {
         cartId,
       });
@@ -125,8 +103,6 @@ function SubmitButton({
       shopifyQueryClient.invalidateQueries({
         queryKey,
       });
-
-      // shopifyQueryClient.setQueryData(queryKey, cart)
     },
   });
 
@@ -138,25 +114,7 @@ function SubmitButton({
   } = useMutation({
     mutationFn: createCartMutationFn,
     mutationKey: ["createCart", cartId],
-    onError: (error, variables, context?: CreateCartContext) => {
-      // An error happened!
-      // console.log("onError", { error, variables, context });
-      // console.log(`rolling back optimistic update with id ${context?.id}`);
-    },
-    onMutate: (variables): CreateCartContext => {
-      // A mutation is about to happen!
-      // console.log("onMutate", { variables });
-
-      // Optionally return a context containing data to use when for example rolling back
-      return { id: 1 };
-    },
-    onSettled: (data, error, variables, context) => {
-      // Error or success... doesn't matter!
-      // console.log("onSettled", { data, error, variables, context });
-    },
-    onSuccess: (data, variables, context) => {
-      // console.log("onSuccess", { data, variables, context });
-
+    onSuccess: (data) => {
       const { cartCreate } = data;
 
       if (cartCreate) {
