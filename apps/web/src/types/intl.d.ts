78683f543028f1798477f03ab8c626fc3b3034a9
Karl Podger <karl.podger@primeordinal.com>
2024-01-25 11:14:24 +0000

    fix: Add type information for stage 4 Intl methods

---
+++ apps/web/src/types/intl.d.ts
@@ -0,0 +1,17 @@
+declare namespace Intl {
+    function getCanonicalLocales(locales: string | string[]): string[];
+
+    class RelativeTimeFormat {
+        constructor(locale: string);
+
+        // TODO Add other properties/methods
+    }
+    // const RelativeTimeFormat: any; // Use this instead of the class if you don't want to declare all properties/methods
+
+    class ListFormat {
+        constructor(locale: string);
+
+        // TODO Add other properties/methods
+    }
+    // const ListFormat: any; // Use this instead of the class if you don't want to declare all properties/methods
+}
