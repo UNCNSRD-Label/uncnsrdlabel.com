f43021b38de81a93e0f7cbd3eb5222a066e67e6a
Karl Podger <karl.podger@primeordinal.com>
2024-03-04 22:05:14 +0000

    fix: Account signed-out layout

--- apps/web/src/app/[lang]/account/layout.tsx
+++ apps/web/src/app/[lang]/account/layout.tsx
@@ -15,13 +15,20 @@ export default async function AccountLayout({
   const customerAccessToken = cookies().get("customerAccessToken")?.value;
 
   return (
-    <div className="sm:bg-dark bg-light relative grid max-w-[100dvw] gap-x-8 gap-y-4 pt-16 sm:bg-[url('/images/backgrounds/MAV7957-Edit.jpg')] sm:bg-cover sm:bg-fixed sm:px-8 md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_fit-content(48rem)_1fr]">
-      <main className={cn("sm:fill-light relative grid content-start sm:order-2 sm:pb-32")}>
+    <div className="sm:bg-dark bg-light relative grid max-w-[100dvw] gap-x-8 gap-y-4 pt-16 sm:bg-[url('/images/backgrounds/MAV7957-Edit.jpg')] sm:bg-cover sm:bg-fixed sm:px-8 md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_fit-content(48rem)_1fr] md:[grid-template-areas:'start_main_end']">
+      <main
+        className={cn(
+          "sm:fill-light relative grid content-start sm:order-2 sm:mb-0 pb-24 sm:pb-32 md:[grid-area:main]",
+          {
+            "pb-0": !!customerAccessToken === true,
+          }
+        )}
+      >
         {children}
       </main>
       {!!customerAccessToken === true && (
         <Nav
-          className="sm:order-1 lg:min-w-96 lg:justify-self-end pb-32"
+          className="pb-24 sm:pb-32 md:order-1 sm:mb-0 sm:[grid-area:start] lg:min-w-96 lg:justify-self-end"
           dictionary={dictionary}
           lang={lang}
         />
