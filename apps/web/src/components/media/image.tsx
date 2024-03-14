d0dcbffe044540c7e8b58733be253fcdafe52a84
Karl Podger <karl.podger@primeordinal.com>
2024-03-14 11:00:33 +0000

    hotfix: Convert Image to sync component, disable blurDataURL ability

--- apps/web/src/components/media/image.tsx
+++ apps/web/src/components/media/image.tsx
@@ -1,32 +1,32 @@
 // import { onLoad } from "@uncnsrdlabel/lib";
 import NextImage, { type ImageProps } from "next/image";
 
-async function getBase64EncodedPlaceholder(url?: string) {
-  if (!url) {
-    return;
-  }
+// async function getBase64EncodedPlaceholder(url?: string) {
+//   if (!url) {
+//     return;
+//   }
 
-  const response = await fetch(url);
+//   const response = await fetch(url);
 
-  if (!response.ok) {
-    return;
-  }
+//   if (!response.ok) {
+//     return;
+//   }
 
-  const blob = await response.arrayBuffer();
+//   const blob = await response.arrayBuffer();
 
-  return `data:${response.headers.get("content-type")};base64,${Buffer.from(blob).toString("base64")}`;
-}
+//   return `data:${response.headers.get("content-type")};base64,${Buffer.from(blob).toString("base64")}`;
+// }
 
-export async function Image({
+export function Image({
   revealEffect = false,
   ...props
 }: ImageProps & { delay?: number; revealEffect?: boolean }) {
-  const blurDataURL = await getBase64EncodedPlaceholder(props.blurDataURL);
+  // const blurDataURL = await getBase64EncodedPlaceholder(props.blurDataURL);
 
   return (
     <NextImage
       {...props}
-      blurDataURL={blurDataURL}
+      // blurDataURL={blurDataURL}
       className={props.className}
       // onLoad={(event) => {
       //   const img = event.currentTarget as HTMLImageElement;
