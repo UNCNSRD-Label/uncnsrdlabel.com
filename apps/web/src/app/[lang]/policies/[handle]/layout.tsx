b476b686ba040cf69b5288e4bae3d64f07e56bdd
Karl Podger <karl.podger@primeordinal.com>
2024-03-18 22:52:52 +0000

    chore: Set matching fallback colour for image

--- apps/web/src/app/[lang]/policies/[handle]/layout.tsx
+++ apps/web/src/app/[lang]/policies/[handle]/layout.tsx
@@ -7,7 +7,7 @@ export default function PoliciesLayout({
 }: PropsWithChildren<LayoutProps>) {
   return (
     <>
-      <main className="sm:bg-dark bg-light relative grid max-w-[100dvw] gap-x-8 gap-y-4 pb-32 pt-[calc(theme(spacing.safeTop)_+_theme(spacing.16))] sm:bg-[url('/images/backgrounds/MAV7957-Edit.jpg')] sm:bg-cover sm:bg-fixed sm:px-8 md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_fit-content(48rem)_1fr]">
+      <main className="bg-light relative grid max-w-[100dvw] gap-x-8 gap-y-4 pb-32 pt-[calc(theme(spacing.safeTop)_+_theme(spacing.16))] sm:bg-orange-700 sm:bg-[url('/images/backgrounds/MAV7957-Edit.jpg')] sm:bg-cover sm:bg-fixed sm:px-8 md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_fit-content(48rem)_1fr]">
         {children}
       </main>
       <Logo />
